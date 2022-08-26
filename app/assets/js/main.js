bfe20c4e0ce1244b00025d25ddeacdf0698fea62
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2022-08-26 14:32:10 +0800

    Добавил окно куки

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -64,6 +64,15 @@ $(document).ready(() => {
 
 });
 
+if (document.querySelector(".warning")) {
+  const container = document.querySelector(".warning");
+  const apply = container.querySelector(".warning__apply");
+  apply.addEventListener("click", () => {
+    container.classList.remove("warning--active");
+    document.cookie = "warning=true; max-age=2592000; path=/";
+  });
+};
+
 // ======== Маска для телефона ===============
 document.addEventListener("DOMContentLoaded", function () {
   var phoneInputs = document.querySelectorAll('input[type="tel"]');
